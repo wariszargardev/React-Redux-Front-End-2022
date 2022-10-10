db8e7a7ec6546f510b14be009208af18d0d4190e
wariszargardev <wariszargardev@gmail.com>
2022-10-10 22:05:45 +0500

    Logger setup for logging state

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,9 @@
 import redux, {createStore} from 'redux'
 import {cakeReducer} from "./reducer/cakeReducer";
+import {logger} from "redux-logger/src";
+import {applyMiddleware} from "redux";
 
-const store = createStore(cakeReducer)
+const store = createStore(cakeReducer,
+    applyMiddleware(logger))
 
 export default store
